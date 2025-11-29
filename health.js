35b9a352ffe921cd4c31038579e9e0ba16adf6de
Sadaf_Takia <takiasadaf21@gmail.com>
2025-11-30 00:34:21 +0600

    added health forum

--- health.js
+++ health.js
@@ -1,15 +1,16 @@
-// Toggle main menu
+
 document.getElementById("menu-toggle").addEventListener("click", function () {
   document.getElementById("nav-links").classList.toggle("active");
 });
 
-// Mobile dropdown open on click
+
 document.querySelectorAll(".drpdwn > a").forEach(function (dropdownLink) {
   dropdownLink.addEventListener("click", function (e) {
     if (window.innerWidth <= 768) {
-      e.preventDefault(); // prevent link default
+      e.preventDefault(); 
       let dropdown = this.nextElementSibling;
       dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
     }
   });
 });
+
